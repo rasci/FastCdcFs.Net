d92aa5e49f8f8495638897efe3ebaf8cb82b2d98
PASCAL SELLE <pascal.selle@agilent.com>
2025-10-03 13:00:23 +0200

    added readme to nuget package

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set VERSION=1.0.0-alpha02
+set VERSION=1.0.0-alpha03
 
 REM test before packing
 
@@ -11,5 +11,7 @@ REM packing
 
 dotnet pack -c Release -o publish FastCdcFs.Net -p:Version=%VERSION% || exit /b 1
 dotnet pack -c Release -o publish FastCdcFs.Net.Shell -p:Version=%VERSION% || exit /b 1
+
+REM   dotnet nuget push "publish/*" --api-key %NUGET_API_KEY% --source https://api.nuget.org/v3/index.json --skip-duplicate || exit /b 1
 
 endlocal
