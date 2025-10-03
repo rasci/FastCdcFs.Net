a1ffb25ba6dcd50169dec4551bef4d0162f05aa2
PASCAL SELLE <pascal.selle@agilent.com>
2025-10-03 12:34:14 +0200

    test before pack

--- build.cmd
+++ build.cmd
@@ -3,6 +3,12 @@ setlocal
 
 set VERSION=1.0.0-alpha02
 
+REM test before packing
+
+dotnet test || exit /b 1
+
+REM packing
+
 dotnet pack -c Release -o publish FastCdcFs.Net -p:Version=%VERSION% || exit /b 1
 dotnet pack -c Release -o publish FastCdcFs.Net.Shell -p:Version=%VERSION% || exit /b 1
 
